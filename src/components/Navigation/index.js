bc9477388c446a8e7b646b17cb8f6c6aa4e464b9
Dimitrios Lytras <dnlytras@gmail.com>
2019-02-02 20:45:40 +0200

    Update navigation highlight condition

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,33 +1,59 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { PureComponent } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 
 import * as ROUTES from 'constants/routes';
 import { Menu } from 'components/Ant';
 import { AuthUserContext } from 'components/Session';
 import SignOutButton from 'components/SignOut';
 
-const NavigationAuth = () => (
-  <Menu mode="horizontal" theme="dark">
-    <Menu.Item>
-      <Link to={ROUTES.HOME}>Activities</Link>
-    </Menu.Item>
-    <Menu.Item>
-      <Link to={ROUTES.USERS}>Users</Link>
-    </Menu.Item>
-    <Menu.Item>
-      <Link to={ROUTES.CALENDAR}>Calendar</Link>
-    </Menu.Item>
-    <Menu.Item>
-      <Link to={ROUTES.ACCOUNT}>Account</Link>
-    </Menu.Item>
+class NavigationAuthBase extends PureComponent {
+  constructor(props) {
+    super(props);
+    const { pathname } = props.location;
+    const cleanPathnane = pathname.replace(/\//g, '');
+    this.state = {
+      active: cleanPathnane,
+    };
+  }
 
-    <Menu.Item className="pull-right">
-      <div>
-        <SignOutButton />
-      </div>
-    </Menu.Item>
-  </Menu>
-);
+  handleClick = e => {
+    this.setState({
+      active: e.key,
+    });
+  };
+
+  render() {
+    return (
+      <Menu
+        mode="horizontal"
+        theme="dark"
+        onClick={this.handleClick}
+        selectedKeys={[this.state.active]}
+      >
+        <Menu.Item key="activities">
+          <Link to={ROUTES.HOME}>Activities</Link>
+        </Menu.Item>
+        <Menu.Item key="users">
+          <Link to={ROUTES.USERS}>Users</Link>
+        </Menu.Item>
+        <Menu.Item key="calendar">
+          <Link to={ROUTES.CALENDAR}>Calendar</Link>
+        </Menu.Item>
+        <Menu.Item key="account">
+          <Link to={ROUTES.ACCOUNT}>Account</Link>
+        </Menu.Item>
+
+        <Menu.Item className="pull-right">
+          <div>
+            <SignOutButton />
+          </div>
+        </Menu.Item>
+      </Menu>
+    );
+  }
+}
+
+const NavigationAuth = withRouter(NavigationAuthBase);
 
 const NavigationNonAuth = () => null;
 
